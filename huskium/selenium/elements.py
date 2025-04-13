7dcfcdd2ac653ec970e60237c51a43ad103d8aa7
__johnny__ <johnny071531@gmail.com>
2025-04-13 14:25:58 +0800

    update generic P

--- huskium/selenium/elements.py
+++ huskium/selenium/elements.py
@@ -30,7 +30,7 @@ from .page import Page
 LOGGER = logging.getLogger(__name__)
 LOGGER.addFilter(LogConfig.PREFIX_FILTER)
 
-P = TypeVar('P', bound=Page[Any, Any], default=Page[Any, Any])
+P = TypeVar('P', bound=Page[Any, Any], default=Page)
 
 
 class Elements(Generic[P, WD, WE]):
